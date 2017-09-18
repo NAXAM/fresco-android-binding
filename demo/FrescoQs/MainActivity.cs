f730264df6c109cec9f098661252956016ab9da5
Vu Duc Tuyen <tuyen@naxam.net>
2017-09-19 00:57:58 +0700

    Simple sample app for Fresco v1.5.0

---
+++ demo/FrescoQs/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Com.Facebook.Drawee.View;
+using Android.Net;
+
+namespace FrescoQs
+{
+    [Activity(Label = "FrescoQs", MainLauncher = true, Theme = "@style/AppTheme", Icon = "@mipmap/ic_launcher")]
+    public class MainActivity : AppCompatActivity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+			Uri uri = Uri.Parse("https://avatars1.githubusercontent.com/u/25535951");
+			SimpleDraweeView draweeView = (SimpleDraweeView)FindViewById(Resource.Id.my_image_view);
+			draweeView.SetImageURI(uri);
+        }
+    }
+}
+
---
+++ demo/FrescoQs/MyApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Com.Facebook.Drawee.Backends.Pipeline;
+
+namespace FrescoQs
+{
+    [Application]
+    public class MyApplication : Application
+    {
+        public MyApplication(IntPtr handle, Android.Runtime.JniHandleOwnership transfer) : base(handle, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            Fresco.Initialize(this);
+        }
+    }
+}
